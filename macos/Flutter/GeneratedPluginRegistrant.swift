d2f53f75a63620a534b367b2a88aa3ca2f7a77e8
Zulfiqor <siapaa249@gmail.com>
2026-04-30 21:33:57 +0700

    ✨ feat: add package

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import mmkv_ios
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MMKVPlugin.register(with: registry.registrar(forPlugin: "MMKVPlugin"))
 }
