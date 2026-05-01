2b862c1542e3a5235244a27e149f07704fa493ff
Zulfiqor <siapaa249@gmail.com>
2026-05-02 01:03:17 +0700

    ✨ feat: add detail page

---
+++ lib/presentation/views/pages/detail_page.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:test_transisi/core/theme/app_colors.dart';
+import 'package:test_transisi/presentation/viewmodels/employee/employee_view_model.dart';
+import 'package:provider/provider.dart';
+
+class DetailPage extends StatefulWidget {
+  final int id;
+
+  const DetailPage({super.key, required this.id});
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<EmployeeViewModel>().fetchDetail(widget.id);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final data = context.watch<EmployeeViewModel>();
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        titleSpacing: 0,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(Icons.arrow_back),
+        ),
+        actions: <Widget>[
+          IconButton(onPressed: () {}, icon: Icon(Icons.star_border_outlined)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+        ],
+      ),
+      body: data.isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 300,
+                    color: AppColors.primary,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(height: 50),
+                        data.detail!.avatar.isNotEmpty
+                            ? Padding(
+                                padding: EdgeInsets.only(bottom: 20),
+                                child: CircleAvatar(
+                                  radius: 80,
+                                  backgroundColor: Colors.white,
+                                  backgroundImage: NetworkImage(
+                                    data.detail!.avatar,
+                                  ),
+                                ),
+                              )
+                            : Icon(
+                                Icons.person,
+                                size: 170,
+                                color: Colors.white,
+                              ),
+                        Text(
+                          "${data.detail!.firstName} ${data.detail!.lastName}",
+                          style: TextStyle(color: Colors.white, fontSize: 25),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColors.grey.withValues(alpha: 0.1),
+                          spreadRadius: 2,
+                          blurRadius: 4,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.person, size: 25, color: AppColors.primary),
+                        SizedBox(width: 30),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                data.detail!.firstName,
+                                style: Theme.of(context).textTheme.labelMedium,
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                data.detail!.lastName,
+                                style: Theme.of(context).textTheme.labelSmall,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Icon(Icons.message, color: AppColors.grey, size: 25),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColors.grey.withValues(alpha: 0.1),
+                          spreadRadius: 2,
+                          blurRadius: 4,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.email, size: 25, color: AppColors.primary),
+                        SizedBox(width: 30),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                data.detail!.email,
+                                style: Theme.of(context).textTheme.labelMedium,
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                data.detail!.firstName,
+                                style: Theme.of(context).textTheme.labelSmall,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
